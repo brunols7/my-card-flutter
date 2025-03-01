8fbdfb87313ecbdafaa98b63ec786edda46d2814
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2025-03-01 15:26:14 -0300

    My Card

--- lib/main.dart
+++ lib/main.dart
@@ -13,36 +13,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: Colors.green,
-                  )
-                ]
+              Text(
+                'Bruno Leonardo',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'
+                )
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize:20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source Code Pro'
+                )
               ),
               Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.blue
-              )
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(width: 10),
+                    Text('11 99332-4970',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 18,
+                      fontFamily: 'Source Code Pro'
+                    )
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'bruno.lsilva1508@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 18,
+                        fontFamily: 'Source Code Pro'
+                      )
+                    )
+                  ],
+                )
+              ),
             ],
-          ),
+          )
         ),
       ),
     );
